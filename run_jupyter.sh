17555904852795680383e853d5971008bf114c8b
csongor <csongor.varady@gmail.com>
2026-04-22 18:33:27 +0300

    Minor update

--- run_jupyter.sh
+++
@@ -1,9 +0,0 @@
-
-
-PORT="$1"
-echo $PORT
-
-if [ -z "$BUILD_PROD" ]; then
-   PORT=8891
-fi
-export PYTHONPATH="$PYTHONPATH:$(pwd)" && jupyter lab --NotebookApp.iopub_data_rate_limit=1.0e10 --ip=0.0.0.0 --port=$PORT
